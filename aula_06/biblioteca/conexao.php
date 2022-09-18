030cbfc711905acd474ab68f30e1d14bb2429ba2
Welson Douglas Oliveira Gomes <welsondouglasgomes@gmail.com>
2022-09-18 12:24:29 -0300

    alteracao

--- aula_06/biblioteca/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-//conexao
-$username = 'root';
-$password = '';
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=aula', $username, $password);
-} catch(PDOException $e) {
-    echo 'ERROr: ' . $e->getMessage();
-}
