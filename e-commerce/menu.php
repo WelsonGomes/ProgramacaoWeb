15ec6b7e3297690d66c13e5df31c5610b9eb65a0
Welson Douglas Oliveira Gomes <welsondouglasgomes@gmail.com>
2022-11-27 16:25:37 -0300

    a
    a

--- e-commerce/menu.php
+++ e-commerce/menu.php
@@ -8,18 +8,18 @@
     <title>Menu</title>
 </head>
 <body>
-<div class="container-fluid" style="background-color: #FFF159;">
+<div class="container-fluid" style="background-color: #5368A6;">
     <div class="container" style="height: 100px;">
         <div class="row">
             <!-- LOGO DO ECOMERCE -->
-            <div class="col-2" style="background-color: #FFF159; height: 100px;">
+            <div class="col-2" style="background-color: #5368A6; height: 100px;">
                 <img class="row col-12 m-auto mt-2" src="_logo/logo.png" alt="Logo do e-commerce">
                 <div class="row col-8 m-auto mt-2 local">
                     <img src="_icones/localizacao.png" alt="">
                 </div>
             </div>
             <!-- MENU DO ECOMERCE -->
-            <div class="col-7" style="background-color: #FFF159; height: 100px;">
+            <div class="col-7" style="background-color: #5368A6; height: 100px;">
                 <div class="row m-auto mt-2 col-12">
                     <input type="search" name="buscar" id="buscar" placeholder="O que você esta procurando">
                 </div>
@@ -69,7 +69,7 @@
                 </div>
             </div>
             <!-- LOGIN DO ECOMERCE -->
-            <div class="col-3" style="background-color: #FFF159; height: 100px;">
+            <div class="col-3" style="background-color: #5368A6; height: 100px;">
                 <div class="row col-11 m-auto mt-2 local">
                     <i class="fa-solid fa-tag col-1 m-0 p-0 pt-1"></i>
                     <p class="col-9 m-0 p-0" style="font-size: 1em; text-align: center;">Ofertas por tempo limitado</p>
